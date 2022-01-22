4b1ea0a52424c06117e17dc220e5d84ee33195a8
dronperminov <dronperminov@gmail.com>
2022-01-22 13:29:23 +0300

    add more simple median-of-two-sorted-arrays problem solve

--- median-of-two-sorted-arrays.py
+++ median-of-two-sorted-arrays.py
@@ -2,29 +2,33 @@ from typing import List
 
 
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
+    def find_kth(self, nums1, nums2, start1, end1, start2, end2, k) -> float:
+        while start1 <= end1 and start2 <= end2:
+            mid1, mid2 = (start1 + end1) // 2, (start2 + end2) // 2
 
-        n1, n2 = len(nums1), len(nums2)
-        left, right = 0, n1
-
-        while True:
-            mid1 = (left + right) // 2
-            mid2 = (n1 + n2) // 2 - mid1
-
-            l1 = float('-inf') if mid1 == 0 else nums1[mid1 - 1]
-            l2 = float('-inf') if mid2 == 0 else nums2[mid2 - 1]
-
-            r1 = float('inf') if mid1 == n1 else nums1[mid1]
-            r2 = float('inf') if mid2 == n2 else nums2[mid2]
-
-            if l1 > r2:
-                right = mid1 - 1
-            elif l2 > r1:
-                left = mid1 + 1
+            if mid1 + mid2 < k:
+                if nums1[mid1] > nums2[mid2]:
+                    start2 = mid2 + 1
+                else:
+                    start1 = mid1 + 1
             else:
-                return min(r1, r2) if (n1 + n2) % 2 else (max(l1, l2) + min(r1, r2)) / 2
+                if nums1[mid1] > nums2[mid2]:
+                    end1 = mid1 - 1
+                else:
+                    end2 = mid2 - 1
+
+        return nums2[k - start1] if start1 > end1 else nums1[k - start2]
+
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        n, m = len(nums1), len(nums2)
+        k = (n + m) // 2
+
+        if (n + m) % 2:
+            return self.find_kth(nums1, nums2, 0, n - 1, 0, m - 1, k)
+
+        v1 = self.find_kth(nums1, nums2, 0, n - 1, 0, m - 1, k)
+        v2 = self.find_kth(nums1, nums2, 0, n - 1, 0, m - 1, k - 1)
+        return (v1 + v2) / 2
 
 
 if __name__ == '__main__':
@@ -34,3 +38,4 @@ if __name__ == '__main__':
     assert solution.findMedianSortedArrays([], [1, 2, 3]) == 2
     assert solution.findMedianSortedArrays([1, 2], [3, 4]) == 2.5
     assert solution.findMedianSortedArrays([3, 4], [1, 2]) == 2.5
+    assert solution.findMedianSortedArrays([1, 2, 2], [1, 2, 3]) == 2
